ebad12d24e996d0fa028fa6cd348d44b5440d006
zackad <zackad.dev@gmail.com>
2017-02-15 17:25:22 +0700

    vue application initiation

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+/**
+ * Globar variable and configuration
+ */
+var DEBUG = true;
+
+/*!
+ * Vue Application
+ */
+var app = new Vue({
+    el: '#app',
+    data: {
+        server: [],
+        input: {
+            name: "",
+            url: ""
+        },
+        info: {
+            detail: '',
+            summary: ''
+        }
+    },
+    methods: {
+        addWMSHost: addWMSHost,
+        clearLocalStorage: clearLocalStorage,
+        editItem: editItem,
+        removeItem: removeItem,
+        getCapabilities: getWMSCapabilities,
+        showList: getLayerList
+    }
+});
+
+checkExistingHost();
